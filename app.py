8bae09c7ae6252403353f28faf05a370dcc7391a
offers-png <offers@dealdily.com>
2025-10-07 18:02:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.post("/clip")
+async def clip(video_file: UploadFile = File(...)):
+    return {"filename": video_file.filename}
+
 
